fac7483d4893a3025d7e9983e6d58c4a91e833d1
Imanol Rodriguez <133455650+IMANOLRR@users.noreply.github.com>
2025-03-13 14:29:33 -0400

    Delete LifeCall directory

--- LifeCall/LifeCall/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LifeCall.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
