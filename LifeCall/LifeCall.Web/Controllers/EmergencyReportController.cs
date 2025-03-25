7259c3050a54bc75fc91adc1b99f106ce58828ee
Imanol Rodriguez <imanol_2005@icloud.com>
2025-03-25 16:37:17 -0400

    Correction
    Correction on methods

--- LifeCall/LifeCall.Web/Controllers/EmergencyReportController.cs
+++ LifeCall/LifeCall.Web/Controllers/EmergencyReportController.cs
@@ -16,24 +16,30 @@ namespace LifeCall.Web.Controllers
 
         public async Task<IActionResult> Reports()
         {
-           var reports = await _context.EmergencyReports.ToListAsync();
+            var reports = await _context.EmergencyReports.ToListAsync();
             return View(reports);
         }
 
-        public IActionResult Create() => View();
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(EmergencyReport report)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(report);
+                _context.EmergencyReports.Add(report);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Reports));
             }
             return View(report);
         }
 
+        [HttpGet]
         public async Task<IActionResult> Edit(int id)
         {
             var report = await _context.EmergencyReports.FindAsync(id);
@@ -43,5 +49,65 @@ namespace LifeCall.Web.Controllers
             }
             return View(report);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, EmergencyReport report)
+        {
+            if (id != report.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(report);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Reports));
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.EmergencyReports.Any(e => e.Id == report.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            return View(report);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var report = await _context.EmergencyReports.FindAsync(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+            return View(report);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var report = await _context.EmergencyReports.FindAsync(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            _context.EmergencyReports.Remove(report);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Reports));
+        }
+
     }
 }
